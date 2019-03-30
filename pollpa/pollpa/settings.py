9c458f7259f13ffd278c73e5594212a8e956a4ac
jeffreyshen19 <jshen@andover.edu>
2019-03-30 18:08:03 -0400

    add frontend structure

--- pollpa/pollpa/settings.py
+++ pollpa/pollpa/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls.apps.PollsConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,6 @@ ROOT_URLCONF = 'pollpa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
