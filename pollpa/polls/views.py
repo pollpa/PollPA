aca19b919fb478bd1f0d4936308a8bf6cf378e27
jeffreyshen19 <jshen@andover.edu>
2019-03-31 16:45:48 -0400

    add poll questions

--- pollpa/polls/views.py
+++ pollpa/polls/views.py
@@ -6,4 +6,41 @@ def index(request):
     return render(request, 'polls/index.html', {})
 
 def poll(request, poll_id):
-    return render(request, 'polls/poll.html', {"poll_id": poll_id})
+    return render(request, 'polls/poll.html', {
+        "poll_id": poll_id,
+        "completed": False,
+        "questions": [{
+            "title": "Question 1 (Checkbox)?",
+            "description": "Testing",
+            "type": "checkbox",
+            "options": [{
+                "title": "a",
+                "description": "This is a description."
+            },
+            {
+                "title": "b",
+                "description": ""
+            },
+            {
+                "title": "c",
+                "description": ""
+            }]
+        },
+        {
+            "title": "Question 2 (Radio)?",
+            "description": "Testing",
+            "type": "radio",
+            "options": [{
+                "title": "a",
+                "description": ""
+            },
+            {
+                "title": "b",
+                "description": "This is a description"
+            },
+            {
+                "title": "c",
+                "description": ""
+            }]
+        }]
+    })
