51dfe700a21043ac10c88a0f7a461ab5200062f4
jeffreyshen19 <jshen@andover.edu>
2019-03-31 13:44:28 -0400

    add rough css structure

--- pollpa/polls/views.py
+++ pollpa/polls/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-    return render(request, 'polls/base.html', {})
+    return render(request, 'polls/index.html', {})
