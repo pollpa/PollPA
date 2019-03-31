c0f45d1cb6b022e44467db8379f1a1deb1c56497
jeffreyshen19 <jshen@andover.edu>
2019-03-31 15:49:35 -0400

    add /poll/xxxx url

--- pollpa/polls/views.py
+++ pollpa/polls/views.py
@@ -4,3 +4,6 @@ from django.template import loader
 
 def index(request):
     return render(request, 'polls/index.html', {})
+
+def poll(request, poll_id):
+    return render(request, 'polls/poll.html', {"poll_id": poll_id})
