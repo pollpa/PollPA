f6e93f2a1975978a90319a6934f8896e7020d0bc
R. Miles McCain <x1-key@sendmiles.email>
2019-03-31 23:54:53 -0400

    Add new urls, views

--- pollpa/polls/urls.py
+++ pollpa/polls/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('poll/<int:poll_id>/', views.poll, name='poll'),
-    path('login/', views.login, name='login'),
+    path('login/', views._login, name='login'),
     path('register/', views.register, name='register'),
     path('reset/', views.reset, name='reset'),
+    path('logout/', views._logout, name="logout"),
+    path('account/', views.account, name="account")
 ]
--- pollpa/polls/views.py
+++ pollpa/polls/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
+from .models import Profile
 
 def index(request):
     return render(request, 'polls/index.html', {})
@@ -45,11 +49,54 @@ def poll(request, poll_id):
         }]
     })
 
-def login(request):
-    return render(request, 'polls/login.html', {})
+def _logout(request):
+    logout(request)
+    return redirect("index")
+
+def _login(request):
+    context = {
+        "error": None
+    }
+    if request.user.is_authenticated:
+        return redirect("index")
+    if request.method == "POST":
+        # emails and usernames are _always_ the same
+        username = request.POST.get('email', None)
+        password = request.POST.get('password', None)
+        if not username.endswith("@andover.edu"):
+            username = username + "@andover.edu"
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("index")
+        else:
+            context["error"] = "Invalid username or password!"
+    return render(request, 'polls/login.html', context)
 
 def register(request):
-    return render(request, 'polls/register.html', {})
+    context = {
+        "error": None
+    }
+    if request.method == "POST":
+        try:
+            username = request.POST.get('username', None)
+            if not username.endswith("@andover.edu"):
+                username = username + "@andover.edu"
+            password = request.POST.get('password', None)
+            grade = int(request.POST.get('grade', None))
+            if grade > 2022 or grade < 2019:
+                raise Exception()
+            user = User.objects.create_user(username, username, password)
+            Profile.objects.create(user=user, grade=grade)
+            login(request, user)
+            return redirect("index")
+        except Exception as e:
+            print(e)
+            context["error"] = "Unable to create your account with the given information; please try again. Either your email is either registered, or you forgot to select a graduation year."
+    return render(request, 'polls/register.html', context)
 
 def reset(request):
     return render(request, 'polls/reset-password.html', {})
+
+def account(request):
+    return render(request, 'polls/account.html', {})
