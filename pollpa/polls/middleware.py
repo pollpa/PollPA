157d78b7bd4d6f87d6cbee462a50c27700e07877
R. Miles McCain <x1-key@sendmiles.email>
2019-04-03 01:54:09 -0400

    Update authtoken middleware

--- pollpa/polls/middleware.py
+++ pollpa/polls/middleware.py
@@ -9,11 +9,13 @@ class AuthTokenMiddleware():
         response = self.get_response(request)
 
         token_id = None
+        token = None
         if request.GET.get("token", None) != None:
             token_id = request.GET.get("token", None)
         if request.POST.get("token", None) != None:
             token_id = request.POST.get("token", None)
-        token = AuthToken.objects.filter(identifier=token_id).first()
+        if token_id != None:
+            token = AuthToken.objects.filter(identifier=token_id).first()
         if token != None:
             user = token.get_user_and_activate()
             if user != None:
