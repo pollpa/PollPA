63bbecd6bc8caa62983db756d8780ac6bc96c865
R. Miles McCain <x1-key@sendmiles.email>
2019-04-06 14:54:38 -0400

    Update gitignore, add local settings

---
+++ pollpa/pollpa/local_settings.py
@@ -0,0 +1,16 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = 'dont worry this is not the prod secret'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3'),
+    }
+}
