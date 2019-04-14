aeb3ea7af1aaacaa144fb6e8ecf6860e835e51f0
jeffreyshen19 <jshen@andover.edu>
2019-04-14 18:31:49 -0400

    update email

--- pollpa/polls/admin.py
+++ pollpa/polls/admin.py
@@ -29,7 +29,7 @@ def welcome_poll(modeladmin, request, queryset):
         for auth in AuthorizedEmail.objects.all():
             if auth.email not in user_emails:
                 to_grade_tuple.append((auth.email, auth.grade))
-        send_many_emails("Parietal rules", "introduction", to_grade_tuple, {"poll": poll})
+        send_many_emails("Parietal rules?", "introduction", to_grade_tuple, {"poll": poll})
 
 welcome_poll.short_description = "Welcome non-registered emails using selected poll (parietal rules only)"
 
